54949de018bd6a126f141392a9c165dc8f518e4d
Maho Pacheco <mapach@microsoft.com>
2024-01-04 10:56:21 -0800

    Fixing issues

--- errorhandling/Error-Utils.psm1
+++ errorhandling/Error-Utils.psm1
@@ -100,37 +100,34 @@ function Invoke-TerminatingCommand {
 
 class RadErrorMessageManager {
     # Static property to store error codes
-    static [System.Collections.Generic.Dictionary[string, string]] $ErrorMessages = @{
+    static [System.Collections.HashTable] $ErrorMessages = @{
         "101" = "Error 102: Invalid parameter: {0}. Expected: {1}. Actual: {2}."
         "500" = "An unexpected error occurred while executing the command: [{0}]. Error message: [{1}]"
     }
 
     # Static method to override error codes from a YAML file
     static [void] LoadErrorCodesFromYaml($yamlFilePath) {
-        try {
-            $yamlContent = Get-Content -Path $yamlFilePath -Raw | ConvertFrom-Yaml
-            if ($yamlContent -is [System.Collections.Hashtable]) {
-                [RadErrorMessageManager]::ErrorMessages = $yamlContent
-            } else {
-                Write-Host "Invalid YAML format. Expected a hashtable."
-            }
-        } catch {
-            Write-Host "Error reading YAML file: $_"
+        $yamlContent = Get-Content -Path $yamlFilePath -Raw | ConvertFrom-Yaml
+
+        if ($yamlContent -and $yamlContent.errors) {
+            [RadErrorMessageManager]::ErrorMessages =  $yamlContent.errors
+        } else {
+            Write-Host "Invalid YAML format. Expected a hashtable with errors property."
         }
     }
 }
 
 function Get-RadErrorMessages {
-    return RadErrorMessageManager::$ErrorMessages
+    return [RadErrorMessageManager]::ErrorMessages
 }
 
 function Import-RadErrorsFromYaml {
     param (
         [Parameter(Mandatory=$true)]
-        [string]$YamlFilePath,
+        [string]$YamlFilePath
     )
 
-    return RadErrorMessageManager::LoadErrorCodesFromYaml($YamlFilePath)
+    return [RadErrorMessageManager]::LoadErrorCodesFromYaml($YamlFilePath)
 }
 
 Export-ModuleMember -Function Get-RadErrorMessage
